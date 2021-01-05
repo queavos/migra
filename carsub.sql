ec53be4b4c5feaf968353e4aaa9ceed76026d646
queavos <queavos.dev@gmail.com>
2021-01-05 16:23:36 -0300

    add der

---
+++ carsub.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.careers_subjets
+(
+  facucar_id,materia_id,person_id,carsubj_year,carsubj_shift,
+  carsubj_startdate,carsubj_enddate,carsubj_require,carsubj_evalprocess,
+  carsubj_evalfinals,carsubj_code)
+VALUES(
+facucar_id, materia_id, person_id, carsubj_year,
+carsubj_shift, carsubj_startdate, carsubj_enddate,
+carsubj_require, carsubj_evalprocess, carsubj_evalfinals, carsubj_code);
